1f6ddf325da2695086559edebc5769c0892b9036
Your Name <you@example.com>
2023-10-29 23:57:58 +0300

    transformer

--- src/app/color/color.tsx
+++ src/app/color/color.tsx
@@ -1,18 +1,6 @@
 import React, { useMemo } from 'react';
-import { appStore } from '../../store';
-import { Counter, DebounceInput, EditableList, Input, Number } from '../../share/ui';
-import { Lens, transform, useLens } from '@vovikilelik/react-lens';
-import { CoveredEditableList, CoveredModelLens } from '../../share/ui/editable-list/covered-editable-list';
-
-import * as styles from './app.module.less';
-
-const textRenderer = (value, i) => <Input key={i} value={value} />
-const addText = (value: Lens<string[]>) => value.set(value.get().concat(''));
-
-const counterRenderer = (value, i) => <Counter key={i} value={value} />
-const addCounter = (value: Lens<number[]>) => value.set(value.get().concat(0));
-
-const addTextCoverable = (value: CoveredModelLens<string>) => value.add('Covered');
+import { Input, Number } from '../../share/ui';
+import { Lens, transform } from '@vovikilelik/react-lens';
 
 const colorTransformer = transform<number, string>(
   v => `#${v.toString(16)}`,
